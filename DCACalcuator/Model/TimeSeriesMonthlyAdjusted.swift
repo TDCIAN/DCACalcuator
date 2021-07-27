22f3eb85472a6c56353502a2f01f0d6d037b9c97
TDCIAN <tdcian71@gmail.com>
2021-07-27 20:27:30 +0900

    Add time series monthly adjusted model

---
+++ DCACalcuator/Model/TimeSeriesMonthlyAdjusted.swift
@@ -0,0 +1,32 @@
+//
+//  TimeSeriesMonthlyAdjusted.swift
+//  DCACalcuator
+//
+//  Created by JeongminKim on 2021/07/27.
+//
+
+import Foundation
+
+struct TimeSeriesMonthlyAdjusted: Decodable {
+    var meta: Meta
+    let timeSeries: [String: OHLC]
+}
+
+struct Meta: Decodable {
+    let symbol: String
+    enum CodingKeys: String, CodingKey {
+        case symbol = "2. Symbol"
+    }
+}
+
+struct OHLC: Decodable {
+    let open: String
+    let close: String
+    let adjustedClose: String
+    
+    enum CodingKeys: String, CodingKey {
+        case open = "1. open"
+        case close = "4. close"
+        case adjustedClose = "5. adjusted close"
+    }
+}
