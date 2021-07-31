5de2b7d97a8c0c65e8903898e9d82c08472806ce
TDCIAN <tdcian71@gmail.com>
2021-07-31 14:16:42 +0900

    update currency labels

---
+++ DCACalcuator/Extensions/String+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  String+Extensions.swift
+//  DCACalcuator
+//
+//  Created by JeongminKim on 2021/07/31.
+//
+
+import Foundation
+
+extension String {
+    func addBrackets() -> String {
+        return "(\(self))"
+    }
+}
